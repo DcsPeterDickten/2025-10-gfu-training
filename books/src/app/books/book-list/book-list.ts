e7513edf1f71638c1f134fca09616e501e8d5f5c
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-10-27 15:03:59 +0100

    WIP

--- books/src/app/books/book-list/book-list.ts
+++ books/src/app/books/book-list/book-list.ts
@@ -1,16 +1,34 @@
-import { JsonPipe } from '@angular/common';
+import { JsonPipe, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'books-list',
-  imports: [JsonPipe],
+  imports: [JsonPipe, NgClass],
   templateUrl: './book-list.html',
   styleUrl: './book-list.css',
 })
 export class BookList {
   books: any[] = [
-    { isbn: 1, title: 'Angular 18', author: 'Author 1', price: 10 },
-    { isbn: 2, title: 'Angular 19', author: 'Author 2', price: 20 },
-    { isbn: 3, title: 'Angular 20', author: 'Author 3', price: 30 },
+    {
+      isbn: 1,
+      title: 'Angular 18',
+      author: 'Author 1',
+      price: 8,
+      coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg',
+    },
+    {
+      isbn: 2,
+      title: 'Angular 19',
+      author: 'Author 2',
+      price: 20,
+      coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg',
+    },
+    {
+      isbn: 3,
+      title: 'Angular 20',
+      author: 'Author 3',
+      price: 30,
+      coverUrl: 'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg',
+    },
   ];
 }
