9aa006d4f7811ed7bd389c82d0f7178faa82b6ee
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-10-28 08:41:27 +0100

    WIP

--- books/src/app/app.ts
+++ books/src/app/app.ts
@@ -1,17 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BookList } from './books/book-list/book-list';
+import { Calculator } from './shared/calculator/calculator';
 
 @Component({
   selector: 'books-root',
-  imports: [RouterOutlet, BookList],
+  imports: [RouterOutlet, BookList, Calculator],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  //
-  protected readonly title2 = signal('books :-) '); // reactive state
   title: string = 'books :-)'; // plain state
-
-  // x = `bla ${this.title}   blubb`;
 }
